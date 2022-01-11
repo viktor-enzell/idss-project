a2b18460d72828a83a7d5c2dfc4f2caf045c70e4
viktor-enzell <viktor.enzell@gmail.com>
2022-01-11 16:43:37 +0100

    Merge profitability and UI changes.

--- project/profit_predictor.py
+++ project/profit_predictor.py
@@ -4,30 +4,32 @@ from django.conf import settings
 from project.constants import *
 import matplotlib.pyplot as plt
 
+
 class ProfitPredictor:
     def predict(self, interest_rate, loan_size, value, price, wacc, rent):
-        loan_cost = interest_rate*loan_size
+        loan_cost = interest_rate * loan_size
         own_capital = price - loan_size
         fixed_cost = value - price
         yearly_earnings = utility_rate * rent * 365
 
         years = 10
         x = np.linspace(0, years, 1000)
-        
+
         zero_line = x * 0
-        y = (x * yearly_earnings - x * loan_cost) +  (np.power(1.05, x)-1)*value + fixed_cost
+        y = (x * yearly_earnings - x * loan_cost) + (np.power(1.05, x) - 1) * value + fixed_cost
 
-        wacc_line = (np.power(1 + wacc, x)-1)*price
-        
-        stock_line = (np.power(1 + stock_return, x)-1)*price
-        upper_stock_line = (np.power(1.12, x)-1)*price
-        lower_stock_line = (np.power(0.9, x)-1)*price
+        wacc_line = (np.power(1 + wacc, x) - 1) * price
 
-        plt.plot(x, wacc_line, color = 'red', label = 'Cost of Capital')
-        plt.plot(x, stock_line, color = 'blue', label = 'Stock Return')
-        plt.fill_between(x,upper_stock_line, lower_stock_line, color = 'lightblue')
-        plt.plot(x, y, color = 'green', label = 'Realestate Return')
-        plt.plot(x, zero_line, color = 'black', linewidth=1)
+        stock_line = (np.power(1 + stock_return, x) - 1) * price
+        upper_stock_line = (np.power(1.12, x) - 1) * price
+        lower_stock_line = (np.power(0.9, x) - 1) * price
+
+        plt.clf()
+        plt.plot(x, wacc_line, color='red', label='Cost of Capital')
+        plt.plot(x, stock_line, color='blue', label='Stock Return')
+        plt.fill_between(x, upper_stock_line, lower_stock_line, color='lightblue')
+        plt.plot(x, y, color='green', label='Realestate Return')
+        plt.plot(x, zero_line, color='black', linewidth=1)
         plt.ylabel('€ Return')
         plt.xlabel('Year')
         plt.xlim([0, years])
